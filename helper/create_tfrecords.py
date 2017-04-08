c87007c06ab3cd4b4b1f96187a1aafa3040ba65b
saikatbsk <saikatbsk@gmail.com>
2017-04-08 18:28:50 +0530

    batch training implemented

---
+++ helper/create_tfrecords.py
@@ -0,0 +1,32 @@
+import skimage.io as io
+import os
+from glob import glob
+import tensorflow as tf
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+image_files = glob(os.path.join('../data/indian_celebrities_male', '*.jpg'))
+tfrecords_filename = '../data/indian_celebrities_male.tfrecords'
+
+writer = tf.python_io.TFRecordWriter(tfrecords_filename)
+
+for img_path in image_files:
+    img = io.imread(img_path)
+
+    height = img.shape[0]
+    width = img.shape[1]
+
+    img_raw = img.tostring()
+
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'height': _int64_feature(height),
+        'width': _int64_feature(width),
+        'image_raw': _bytes_feature(img_raw)}))
+
+    writer.write(example.SerializeToString())
+
+writer.close()
--- main.py
+++ main.py
@@ -5,26 +5,50 @@ from utils import *
 from dcgan import DCGAN
 
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_string('log_dir',       'checkpoints', """Path to write logs and checkpoints""")
-tf.app.flags.DEFINE_string('images_dir',    'images',      """Path to save generated images""")
-tf.app.flags.DEFINE_string('data_dir',      'data',        """Path to data directory""")
-tf.app.flags.DEFINE_integer('max_itr',      10000,         """Maximum number of iterations""")
-tf.app.flags.DEFINE_integer('latest_ckpt',  0,             """Latest checkpoint timestamp to load""")
-tf.app.flags.DEFINE_boolean('is_train',     True,          """False for generating only""")
-tf.app.flags.DEFINE_boolean('is_grayscale', False,         """True for grayscale images [not yet implemented]""")
+tf.app.flags.DEFINE_string('log_dir',       'checkpoints',           """Path to write logs and checkpoints""")
+tf.app.flags.DEFINE_string('images_dir',    'images',                """Path to save generated images""")
+tf.app.flags.DEFINE_string('data_dir',      'data',                  """Path to data directory""")
+tf.app.flags.DEFINE_integer('max_itr',      10000,                   """Maximum number of iterations""")
+tf.app.flags.DEFINE_integer('latest_ckpt',  0,                       """Latest checkpoint timestamp to load""")
+tf.app.flags.DEFINE_boolean('is_train',     True,                    """False for generating only""")
+tf.app.flags.DEFINE_boolean('is_grayscale', False,                   """True for grayscale images""")
+tf.app.flags.DEFINE_integer('num_examples_per_epoch_for_train', 300, """number of examples for train""")
+
+CROP_IMAGE_SIZE = 96
+
+def read_decode(batch_size, f_size):
+    files = [os.path.join(FLAGS.data_dir, f) for f in os.listdir(FLAGS.data_dir) if f.endswith('.tfrecords')]
+    fqueue = tf.train.string_input_producer(files)
+    reader = tf.TFRecordReader()
+    _, serialized = reader.read(fqueue)
+    features = tf.parse_single_example(serialized, features={
+        'height': tf.FixedLenFeature([], tf.int64),
+        'width': tf.FixedLenFeature([], tf.int64),
+        'image_raw': tf.FixedLenFeature([], tf.string)})
+
+    image = tf.cast(tf.decode_raw(features['image_raw'], tf.uint8), tf.float32)
+    height = tf.cast(features['height'], tf.int32)
+    width = tf.cast(features['width'], tf.int32)
+
+    image = tf.reshape(image, [height, width, 3])
+    image = tf.image.resize_image_with_crop_or_pad(image, CROP_IMAGE_SIZE, CROP_IMAGE_SIZE)
+    #image = tf.image.random_flip_left_right(image)
+
+    min_queue_examples = FLAGS.num_examples_per_epoch_for_train
+    images = tf.train.shuffle_batch(
+        [image],
+        batch_size=batch_size,
+        capacity=min_queue_examples + 3 * batch_size,
+        min_after_dequeue=min_queue_examples)
+    tf.summary.image('images', images)
+    return tf.subtract(tf.div(tf.image.resize_images(images, [f_size * 2 ** 4, f_size * 2 ** 4]), 127.5), 1.0)
 
 def main(_):
-    dcgan = DCGAN(batch_size=128, f_size=6, z_dim=40,
+    dcgan = DCGAN(batch_size=4, f_size=6, z_dim=40,
         gdepth1=216, gdepth2=144, gdepth3=96,  gdepth4=64,
         ddepth1=64,  ddepth2=96,  ddepth3=144, ddepth4=216)
 
-    """
-    Batch training not implemented yet. Keep exactly 128 images inside data_dir.
-    """
-    input_images = input_data(FLAGS.data_dir,
-        input_height=128, input_width=128,
-        resize_height=96, resize_width=96,
-        is_grayscale=FLAGS.is_grayscale)
+    input_images = read_decode(dcgan.batch_size, dcgan.f_size)
 
     train_op = dcgan.build(input_images, feature_matching=True)
 
